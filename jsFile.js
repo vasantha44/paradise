d893adbb595b077bc2aba0b352350f0b44979415
vasantha44 <108451589+vasantha44@users.noreply.github.com>
2022-07-22 12:02:33 +0530

    Add files via upload

---
+++ jsFile.js
@@ -0,0 +1,90 @@
+ function btn(){
+  window.scrollTo({top:0, behavior: 'smooth'})
+}
+function veg(){
+span.innerHTML=`<div class="container">
+<div class="row">
+<div class="col-lg-3"><h4 class="head_1">Hyd Veg Birayani</h4>
+  <img src="https://bootstrapmade.com/demo/templates/Restaurantly/assets/img/menu/caesar.jpg" class="img_veg"/>
+  </div>
+<div class="col-lg-3"> <h4 class="head_1">Veg Fried Rice</h4>
+  <img src="https://media.istockphoto.com/photos/veg-biryani-or-veg-pulav-or-steamed-rice-closeup-picture-id1067967232?k=20&m=1067967232&s=612x612&w=0&h=4JKgle5k7trdhHLIr4cSr9k7TNnlu7quhWKPLhGwPIY=" class="img_veg"/></div>
+<div class="col-lg-3"> <h4 class="head_1">Mushroom Birayani</h4>
+  <img src="https://media.istockphoto.com/photos/image-of-bowl-of-spicy-kung-pao-chicken-with-mixed-vegetables-spring-picture-id1208954729?k=20&m=1208954729&s=612x612&w=0&h=8SYDx8vsurTEflOIm15hRdf_s5lsIJyacJSf8s3FUD8=" class="img_veg"/></div>
+<div class="col-lg-3"><h4 class="head_1">Paneer curry</h4>
+  <img src="https://media.istockphoto.com/photos/malai-or-achari-paneer-in-a-gravy-made-using-red-gravy-and-green-in-picture-id1085159910?k=20&m=1085159910&s=612x612&w=0&h=ocZXxTscQzYa1FbU1OT-LzndmaAjyVgpg9MAS9mp_YY=" class="img_veg"/>
+</div>
+
+
+
+</div>
+</div>`
+}
+
+function non(){
+    span.innerHTML=`<div class="container">
+<div class="row">
+<div class="col-lg-3"><h4 class="head_1">Hyderabad Birayani</h4>
+  <img src="https://media.istockphoto.com/photos/indian-food-picture-id509568543?k=20&m=509568543&s=612x612&w=0&h=yQH98PyYEO_6h5W0PtBmmR7pabBj5GNuuRULqanJVzw=" class="img_veg"/>
+  </div>
+<div class="col-lg-3"> <h4 class="head_1">Chicken Masala</h4>
+  <img src="https://media.istockphoto.com/photos/gosht-or-lamb-biryani-prepared-in-basmati-rice-served-with-yogurt-dip-picture-id980036840?k=20&m=980036840&s=612x612&w=0&h=EhD69TYhvWTARjuTrIlOqZYs4PKXF-uHRgo1YdAWNZ8=" class="img_veg"/></div>
+<div class="col-lg-3"> <h4 class="head_1">Chicken Wings</h4>
+  <img src="https://media.istockphoto.com/photos/chicken-wings-picture-id532607409?k=20&m=532607409&s=612x612&w=0&h=a-y7Qf-WXmDwsxRn9St69Fshpcp-xOogymfHKHHZBsU=" class="img_veg"/>
+</div>
+<div class="col-lg-3"><h4 class="head_1">Ginger Mutton</h4>
+  <img src="https://media.istockphoto.com/photos/mutton-curry-picture-id494350327?k=20&m=494350327&s=612x612&w=0&h=T5Gv3T-y0s8AKL2UDtvQ5djG6Hxg46VIklTQlekbvww=" class="img_veg"/>
+</div>
+
+
+
+</div>
+</div>`
+
+}
+
+function cakes(){
+    span.innerHTML=`<div class="container">
+<div class="row">
+<div class="col-lg-3"><h4 class="head_1">Butterscotch</h4>
+  <img src="https://media.istockphoto.com/photos/slice-of-cheesecake-with-rosemary-caramel-sauce-xxl-picture-id174615997?k=20&m=174615997&s=612x612&w=0&h=u9xCHF67uhJM92M6MTcj107KMqXb1CIszGCD8GV4MQU=" class="img_veg"/>
+  </div>
+<div class="col-lg-3"> <h4 class="head_1">Chocolate Cake</h4>
+  <img src="https://media.istockphoto.com/photos/dessert-chocolate-cake-picture-id155598375?k=20&m=155598375&s=612x612&w=0&h=wyjDWsZ3Q6PAmyURtSiSaaEC_xjSks2VQp-H2fAanw8=" class="img_veg"/></div>
+<div class="col-lg-3"> <h4 class="head_1"> Stawberry Pastry</h4>
+  <img src="https://media.istockphoto.com/photos/stawberry-cheesecake-picture-id1146488934?k=20&m=1146488934&s=612x612&w=0&h=QoCIP3XOWCYMojZeL32EIuTclq04cI2wV6K25OT8wnk=" class="img_veg"/>
+</div>
+<div class="col-lg-3"><h4 class="head_1">Orange Cake</h4>
+  <img src="https://media.istockphoto.com/photos/freshly-made-mango-cream-cake-on-wooden-table-selective-focus-picture-id1292450859?k=20&m=1292450859&s=612x612&w=0&h=EIXzoizWzeZPtr7ju5_MNca7IJ2T1_dEk3HatDsuj7c=" class="img_veg"/>
+</div>
+
+
+
+</div>
+</div>`
+}
+
+
+function desserts(){
+    span.innerHTML=`<div class="container">
+<div class="row">
+<div class="col-lg-3"><h4 class="head_1">Stawbeery Pie</h4>
+  <img src="https://media.istockphoto.com/photos/cake-with-stawberry-and-creamy-picture-id1333023696?k=20&m=1333023696&s=612x612&w=0&h=9IOxW32bTCVCTROHTtzneXk6rmTCAkf_XmalMdi9U9c=" class="img_veg"/>
+  </div>
+<div class="col-lg-3"> <h4 class="head_1">Apple  crispy</h4>
+  <img src="https://media.istockphoto.com/photos/apple-crumble-picture-id467339919?k=20&m=467339919&s=612x612&w=0&h=BMpYuRiYqlKgQMdXlC8lM2FX5_jVWwnzr71zO26GkV4=" class="img_veg"/></div>
+<div class="col-lg-3"> <h4 class="head_1">Gulab Jamun</h4>
+  <img src="https://media.istockphoto.com/photos/gulab-jamun11-picture-id521803129?k=20&m=521803129&s=612x612&w=0&h=rxy4lr0ByQj6PUf-foL7OKf-3Tjb6Nutg9NbmElMH1w=" class="img_veg"/>
+</div>
+<div class="col-lg-3"><h4 class="head_1">Bread Halwa</h4>
+  <img src="https://media.istockphoto.com/photos/gajar-halwa-sweet-picture-id1156630527?k=20&m=1156630527&s=612x612&w=0&h=Q8WsAprWNwZkkhcarvYfamOlEBewGRxT12xwiKiqNZg=" class="img_veg"/>
+</div>
+
+
+
+</div>
+</div>`
+
+}
+
+
